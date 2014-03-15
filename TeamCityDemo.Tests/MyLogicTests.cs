fa42acbdfafefb6e8b69edcadbd2f6fa8cf0a088
Maor Hayun <maor.hayun@gmail.com>
2014-03-15 23:34:11 +0200

    first commit

---
+++ TeamCityDemo.Tests/MyLogicTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeamCityDemo.Tests
+{
+    [TestClass]
+    public class MyLogicTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PrintNTimes_WhenInputIsNull_ThrowArgumentNullException()
+        {
+            var logic = new MyLogic();
+
+            string input = null;
+
+            logic.PrintNTimes(input, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PrintNTimes_WhenNArgumentIsNegative_ThrowInvalidOperationException()
+        {
+            var logic = new MyLogic();
+
+            string input = "test_value";
+            int n = -1;
+
+            logic.PrintNTimes(input, n);
+
+        }
+
+        //[TestMethod]
+        //public void PrintNTimes_WhenNArgumentIsNegative_ReturnFalse()
+        //{
+        //    var logic = new MyLogic();
+
+        //    string input = "test_value";
+        //    int n = -1;
+
+        //    var result = logic.PrintNTimes(input, n);
+        //    Assert.IsFalse(result);
+        //}
+
+        [TestMethod]
+        public void PrintNTimes_WhenInputAndNAreValid_ReturnTrue()
+        {
+            var logic = new MyLogic();
+
+            string input = "test_value";
+            int n = 3;
+
+            var result = logic.PrintNTimes(input, n);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ TeamCityDemo/MyLogic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityDemo
+{
+    public class MyLogic
+    {
+        public bool PrintNTimes(string input, int n)
+        {
+            if (string.IsNullOrEmpty(input))
+                throw new ArgumentNullException("input");
+
+            if (n < 0)
+                throw new InvalidOperationException("n should be greater than zero");
+            // return false;
+
+            for (var i = 0; i < n; i++)
+            {
+                Console.WriteLine(input);
+            }
+
+            return true;
+        }
+    }
+}

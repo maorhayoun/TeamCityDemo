f12c633c17a44f691253b44bfbf39e38c8bf5a10
Maor Hayun <maor.hayun@gmail.com>
2014-03-18 01:06:26 +0200

    paused execution with 1 sec

--- TeamCityDemo/MyLogic.cs
+++ TeamCityDemo/MyLogic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TeamCityDemo
@@ -16,6 +17,8 @@ namespace TeamCityDemo
             if (n < 0)
              //   throw new InvalidOperationException("n should be greater than zero");
              return false;
+
+            Thread.Sleep(1000);
 
             for (var i = 0; i < n; i++)
             {
